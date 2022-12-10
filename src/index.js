f8ae322ae97df01a8a7737755dafda4987c32881
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2022-12-10 11:17:30 +0200

    finish

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { fetchImages } from './js/fetchApi.js';
+import { renderGallery } from './js/renderGallery.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const refs = {
+ searchForm: document.querySelector('#search-form'),
+ gallery: document.querySelector('.gallery'),
+ loadMoreBtn: document.querySelector('.button--loadMore')
+}
+
+
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+refs.searchForm.addEventListener('submit', createForm);
+refs.loadMoreBtn.addEventListener('click', createLoadMoreBtn);
+
+function createForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+
+  if (query === '') {
+    Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a');
+
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+        if (data.totalHits > perPage) {
+          refs.loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error));
+
+  refs.searchForm.reset();
+}
+
+function createLoadMoreBtn() {
+  page += 1;
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a');
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page >= totalPages) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+
+        Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
