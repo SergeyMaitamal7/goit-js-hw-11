61e1eb161e1c456260a4b5194013e49e499d1cca
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2022-12-10 17:28:04 +0200

    finish 5

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 export { fetchImages };
 
-const axios = require('axios').default;
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '29175457-ea8e2c93dbfac842acac0bec2';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
   return response;
 }
